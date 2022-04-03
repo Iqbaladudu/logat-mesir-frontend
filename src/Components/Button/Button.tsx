3ec577e5007aa3b322d462c194b1a4be8df88bc3
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-04-03 11:23:42 +0200

    Course detailview done fix

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,54 +1,54 @@
-import Link from 'next/link'
+import Link from "next/link";
 import type {
-    ButtonHTMLAttributes,
-    FormEventHandler,
-    MouseEventHandler,
-} from 'react'
-import styles from './Button.module.css'
-import clsx from 'clsx'
+	ButtonHTMLAttributes,
+	FormEventHandler,
+	MouseEventHandler,
+} from "react";
+import styles from "./Button.module.css";
+import clsx from "clsx";
 
 export type Props = {
-    label: string
-    secondary?: Boolean
-    def?: Boolean
-    onClick?: MouseEventHandler<HTMLButtonElement>
-    className?: string
-    type?: ButtonHTMLAttributes<HTMLButtonElement>['type']
-}
+	label: string;
+	secondary?: Boolean;
+	def?: Boolean;
+	onClick?: MouseEventHandler<HTMLButtonElement>;
+	className?: string;
+	type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+};
 
 const Button: React.FC<Props> = ({
-    label,
-    secondary,
-    def,
-    onClick,
-    className,
-    type,
+	label,
+	secondary,
+	def,
+	onClick,
+	className,
+	type,
 }) => {
-    return onClick ? (
-        <button
-            type={type}
-            onClick={onClick}
-            className={clsx(
-                styles.button,
-                className,
-                secondary && styles.buttonsecondary
-            )}
-        >
-            {label}
-        </button>
-    ) : (
-        <button
-            type={type}
-            className={clsx(
-                styles.button,
-                className,
-                def && styles.default,
-                secondary && styles.buttonsecondary
-            )}
-        >
-            {label}
-        </button>
-    )
-}
+	return onClick ? (
+		<button
+			type={type}
+			onClick={onClick}
+			className={clsx(
+				styles.button,
+				className,
+				secondary && styles.buttonsecondary
+			)}
+		>
+			{label}
+		</button>
+	) : (
+		<button
+			type={type}
+			className={clsx(
+				styles.button,
+				className,
+				def && styles.default,
+				secondary && styles.buttonsecondary
+			)}
+		>
+			{label}
+		</button>
+	);
+};
 
-export default Button
+export default Button;
--- src/Containers/Course/CourseDetail/CourseDetail.tsx
+++ src/Containers/Course/CourseDetail/CourseDetail.tsx
@@ -1,73 +1,66 @@
-import Button from '../../../Components/Button/Button'
-import Container from '../../../Components/Container/Container'
-import SplitContainer from '../../Dashboard/SplitContainer/SplitContainer'
-import Line from '/public/whyusline.svg'
-import Image from 'next/image'
-import Card from '../../../Components/Card/Card'
-import VideoIcon from '/public/VideoIcon.svg'
-import clsx from 'clsx'
+import Button from "../../../Components/Button/Button";
+import Container from "../../../Components/Container/Container";
+import SplitContainer from "../../Dashboard/SplitContainer/SplitContainer";
+import Line from "/public/whyusline.svg";
+import Image from "next/image";
+import Card from "../../../Components/Card/Card";
+import VideoIcon from "/public/VideoIcon.svg";
+import clsx from "clsx";
 
 const CourseDetail = () => {
-    return (
-        <section className="overflow-hidden">
-            <Container width="w-screen" height="h-screen" flex="flex flex-col">
-                <div className="flex-1">
-                    <SplitContainer>
-                        <div className="flex-1 flex flex-col items-center">
-                            <div className="flex flex-col items-start gap-[39px]">
-                                <div>
-                                    <p className="font-medium text-[64px] leading-[64px] text-blue-secondary w-[492px]">
-                                        Belajar Amiyah itu Asik
-                                    </p>
-                                </div>
-                                <div>
-                                    <Image src={Line} alt="" />
-                                </div>
-                                <Button
-                                    label="Belajar"
-                                    className="btn w-[275px] h-[67px] shadow-[0_5px_7px_rgba(0,0,0,0.25)] rounded-[20px] leading-[54px] bg-red-lm"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex-1 flex">
-                            <Card className="w-[599px] h-[300px] bg-blue-core rounded-[20px]">
-                                <div className="flex justify-center items-center w-[180px] h-[180px] bg-white rounded-full">
-                                    <Image
-                                        src={VideoIcon}
-                                        height="104"
-                                        width="104"
-                                        alt=""
-                                    />
-                                </div>
-                            </Card>
-                        </div>
-                    </SplitContainer>
-                </div>
-                <div className="flex-1 flex flex-col justify-center items-center gap-[32px]">
-                    <div>
-                        <p className="text-[48px] leading-[72px] font-medium text-blue-secondary">
-                            Materi Belajar
-                        </p>
-                    </div>
-                    <div className="flex flex-row gap-[51px]">
-                        {[1, 2, 3, 4].map((_, index) => (
-                            <Card
-                                customized
-                                className={clsx(
-                                    'w-[237px] h-[106px] bg-red-lm rounded-[10px]'
-                                )}
-                                key={index}
-                            >
-                                <p className="text-white font-medium text-[36px] leading-[53px]">
-                                    Card text
-                                </p>
-                            </Card>
-                        ))}
-                    </div>
-                </div>
-            </Container>
-        </section>
-    )
-}
+	return (
+		<section className="overflow-hidden">
+			<Container width="w-screen" height="h-screen" flex="flex flex-col">
+				<div className="flex-1">
+					<SplitContainer>
+						<div className="flex-1 flex flex-col items-center">
+							<div className="flex flex-col items-start gap-[39px]">
+								<div>
+									<p className="font-medium text-[64px] leading-[64px] text-blue-secondary w-[492px]">
+										Belajar Amiyah itu Asik
+									</p>
+								</div>
+								<div>
+									<Image src={Line} alt="" />
+								</div>
+								<Button
+									label="Belajar"
+									className="btn w-[275px] h-[67px] shadow-[0_5px_7px_rgba(0,0,0,0.25)] rounded-[20px] leading-[54px] bg-red-lm"
+								/>
+							</div>
+						</div>
+						<div className="flex-1 flex">
+							<Card className="w-[599px] h-[300px] bg-blue-core rounded-[20px]">
+								<div className="flex justify-center items-center w-[180px] h-[180px] bg-white rounded-full">
+									<Image src={VideoIcon} height="104" width="104" alt="" />
+								</div>
+							</Card>
+						</div>
+					</SplitContainer>
+				</div>
+				<div className="flex-1 flex flex-col justify-center items-center gap-[32px]">
+					<div>
+						<p className="text-[48px] leading-[72px] font-medium text-blue-secondary">
+							Materi Belajar
+						</p>
+					</div>
+					<div className="flex flex-row gap-[51px]">
+						{[1, 2, 3, 4].map((_, index) => (
+							<Card
+								customized
+								className={clsx("w-[237px] h-[106px] bg-red-lm rounded-[10px]")}
+								key={index}
+							>
+								<p className="text-white font-medium text-[36px] leading-[53px]">
+									Card text
+								</p>
+							</Card>
+						))}
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
 
-export default CourseDetail
+export default CourseDetail;
