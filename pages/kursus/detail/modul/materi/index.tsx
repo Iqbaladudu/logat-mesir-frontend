06144e7b56af997a6406cd59de82297a9c05d094
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-04-04 17:34:17 +0200

    fix

---
+++ pages/kursus/detail/modul/materi/index.tsx
@@ -0,0 +1,7 @@
+import CourseContents from "../../../../../src/Containers/Course/CourseContents/CourseContents";
+
+const materi = () => {
+	return <CourseContents />;
+};
+
+export default materi;
--- src/Components/Layout/Header/Nav/Nav.tsx
+++ src/Components/Layout/Header/Nav/Nav.tsx
@@ -1,16 +1,16 @@
-import NavItem from "../NavItem/NavItem"
+import NavItem from "../NavItem/NavItem";
 
 const Nav = () => {
-    return (
-        <>
-            <nav className="btn-group rounded-lg">
-                <NavItem label="Beranda" href="/" />
-                <NavItem label="Kursus" href="/" />
-                <NavItem label="Daftar" href="/daftar" />
-                <NavItem label="Komunitas" href="/" />
-            </nav>
-        </>
-    )
-}
+	return (
+		<>
+			<nav className="btn-group rounded-lg">
+				<NavItem label="Beranda" href="/" />
+				<NavItem label="Kursus" href="/" />
+				<NavItem label="Daftar" href="/daftar" />
+				<NavItem label="Komunitas" href="/" />
+			</nav>
+		</>
+	);
+};
 
-export default Nav
+export default Nav;
