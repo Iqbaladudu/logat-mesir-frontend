b66567a0903a3a5afc6232faeb2119db1e4b42f8
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-04-08 08:58:48 +0200

    multistep form functionality

--- src/Containers/Register/hooks.ts
+++ src/Containers/Register/hooks.ts
@@ -1,22 +1,22 @@
-import type { FormInputs as RegisterInput } from './interfaces'
-import type { SubmitHandler } from 'react-hook-form'
-import { useForm } from 'react-hook-form'
+import type { FormInputs as RegisterInput } from "./interfaces";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export const useComponentLogic = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<RegisterInput>()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<RegisterInput>();
 
-    const onSubmit: SubmitHandler<RegisterInput> = (data) => {
-        console.log('data::', data)
-    }
+	const onSubmit: SubmitHandler<RegisterInput> = (data) => {
+		console.log("data:", data);
+	};
 
-    return {
-        register,
-        handleSubmit,
-        onSubmit,
-        errors,
-    }
-}
+	return {
+		register,
+		handleSubmit,
+		onSubmit,
+		errors,
+	};
+};
